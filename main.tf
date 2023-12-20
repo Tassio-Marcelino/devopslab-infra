f5be7e008cb5f2eda88f3551d9a88471f67dfcc7
Tassio-Marcelino <146141025+Tassio-Marcelino@users.noreply.github.com>
2023-12-20 19:49:31 -0300

    fix version4

--- main.tf
+++ main.tf
@@ -1,22 +1,21 @@
-# Cria uma VM no Google Cloud
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
 resource "google_compute_instance" "firstvm" {
   name         = "helloworld"
-  machine_type = "n1-standard-1"
-  zone         = "var.zone"
+  machine_type = "n2-standard-2"
+  zone         = var.zone
 
-  # Defini a Imagem da VM
   boot_disk {
     initialize_params {
       image = "ubuntu-2004-focal-v20230213"
     }
   }
 
-  # Habilita rede para a VM com um IP público
   network_interface {
-    network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
-
-    access_config {
-    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
-    }
+    network = "default"
+    access_config {}
   }
 }
