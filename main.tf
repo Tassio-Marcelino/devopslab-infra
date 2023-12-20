163b98bcae8b9b686467295db921ea372191b860
Tassio-Marcelino <146141025+Tassio-Marcelino@users.noreply.github.com>
2023-12-20 19:43:03 -0300

    Fix version2

--- main.tf
+++ main.tf
@@ -1,22 +1,21 @@
-# Cria uma VM no Google Cloud
+# Defina a variável de zona, por exemplo:
+variable "zone" {
+  default = "us-west1-b"
+}
+
 resource "google_compute_instance" "firstvm" {
   name         = "helloworld"
   machine_type = "n2-standard-2"
-  zone         = var.zone
+  zone         = "us-west1-c"  # Substitua pela zona desejada
 
-  # Defini a Imagem da VM
   boot_disk {
     initialize_params {
       image = "ubuntu-2004-focal-v20230213"
     }
   }
 
-  # Habilita rede para a VM com um IP público
   network_interface {
-    network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
-
-    access_config {
-    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
-    }
+    network = "default"
+    access_config {}
   }
 }
