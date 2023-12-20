bec6e06fe4d82e7f81c610b861f1a1546a64ec74
Tassio-Marcelino <146141025+Tassio-Marcelino@users.noreply.github.com>
2023-12-20 19:53:15 -0300

    fix6

--- providers.tf
+++ providers.tf
@@ -1,13 +1,14 @@
+
 terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "5.9.0"
+      version = "4.54.0"
     }
   }
 }
 
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-  project = var.project_id
+  project = "var.project_id"
 }
